df7af16d87f9f5f8980d534ba5b31b4e9d52ef1b
Allison Ventura <aventura@hubspot.com>
2016-04-08 23:35:05 -0400

    connect to mongodb

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+// Create the server
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+// Serving static files from public directory
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// Connect to the database
+mongoose.connect(process.env.MONGOLAB_URI);
+
+// Checking to make sure connection was successful
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we\'re connected!');
+});
+
+// Create the Schema and Model for messages
+var messageSchema = mongoose.Schema({
+  message: String
+});
+var Message = mongoose.model('Message', messageSchema);
+
+
+// Set up our GET and POST requests
+
 app.get('/', function(request, response) {
+  Message.find(function (err, messages) {
+    if (err) return console.error(err);
+      console.log(messages);
+    })
   response.render('pages/index');
+});
+
+app.post('/', function (request, response) {
+  Message.find(function (err, messages) {
+    if (err) return console.error(err);
+      console.log(messages);
+    })
 });
 
 app.listen(app.get('port'), function() {
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,1 @@
-console.log("hi");
+console.log("Let's make a database!");
