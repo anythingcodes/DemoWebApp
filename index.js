6a1d14028e742b665c7d943f4623759993bda867
Allison Ventura <aventura@hubspot.com>
2016-04-09 00:36:46 -0400

    get and post messages

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var mongoose = require('mongoose');
 
 // Create the server
@@ -13,6 +14,9 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// Use body parser to read request bodies
+app.use(bodyParser());
+
 // Connect to the database
 mongoose.connect(process.env.MONGOLAB_URI);
 
@@ -25,26 +29,30 @@ db.once('open', function() {
 
 // Create the Schema and Model for messages
 var messageSchema = mongoose.Schema({
-  message: String
+  messageText: String
 });
 var Message = mongoose.model('Message', messageSchema);
 
+var getAndRenderPostedMessages = function(request, response) {
+  Message.find(function (err, messages) {
+    if (err) return console.error(err);
+    var descendingMessages = messages.reverse();
+    response.render('pages/index', {messages: descendingMessages});
+  })
+}
 
 // Set up our GET and POST requests
 
 app.get('/', function(request, response) {
-  Message.find(function (err, messages) {
-    if (err) return console.error(err);
-      console.log(messages);
-    })
-  response.render('pages/index');
+  getAndRenderPostedMessages(request, response);
 });
 
 app.post('/', function (request, response) {
-  Message.find(function (err, messages) {
+  var newMessage = new Message({ messageText: request.body.message });
+  newMessage.save(function (err, newMessage) {
     if (err) return console.error(err);
-      console.log(messages);
-    })
+    getAndRenderPostedMessages(request, response);
+  });
 });
 
 app.listen(app.get('port'), function() {
